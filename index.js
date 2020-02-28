d5aaebf017e3336457cc85b4536365c201d6cd09
Kacper Walanus <kacper@walanus.com>
2020-02-28 17:28:54 +0100

    notify about approved and rejected pr

--- index.js
+++ index.js
@@ -2,15 +2,43 @@ const Core = require('@actions/core');
 const Github = require('@actions/github');
 const Slack = require('node-slack');
 
-const DefaultFormat = `:rocket: New PR ready for review! :rocket:\nTitle: *{ pull_request.title }*\nAuthor: { pull_request.user.login }\nURL: { pull_request.html_url }`;
+const DefaultPRApprovedFormat = `Pull request *{ pull_request.title }* was approved by { review.user.login } :heavy_check_mark:`;
+const DefaultPRChangesRequestedFormat = `Pull request *{ pull_request.title }* was rejected by { review.user.login } :cry:`;
+const DefaultPRReadyForReviewFormat = `:rocket: New PR ready for review! :rocket:\nTitle: *{ pull_request.title }*\nAuthor: { pull_request.user.login }\nURL: { pull_request.html_url }`;
+
+const fillTemplate = (payload, template) => {
+    let message = template;
+    template.match(/\{(.*?)\}/g).forEach(template => {
+        const templateWithoutBrackets = template
+            .replace(/^\{\s?/, "")
+            .replace(/\s?\}$/, "");
+
+        const keys = templateWithoutBrackets.split(".");
+
+        let value = payload;
+        keys.forEach(key => {
+            try {
+                if (value[key]) {
+                    value = value[key];
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        });
+        message = message.replace(template, value);
+    });
+    return message;
+};
 
 try {
     e = process.env;
     config = {
         channel: e.SLACK_CHANNEL,
-        format: e.FORMAT || DefaultFormat,
         hookUrl: e.SLACK_WEBHOOK,
         ignoreDrafts: e.IGNORE_DRAFTS || true,
+        pr_approved_format: e.PR_APPROVED_FORMAT || DefaultPRApprovedFormat,
+        pr_ready_for_review_format: e.PR_READY_FOR_REVIEW_FORMAT || DefaultPRReadyForReviewFormat,
+        pr_rejected_format: e.PR_REJECTED_FORMAT || DefaultPRChangesRequestedFormat,
         username: e.USERNAME || 'ReadyForReviewBot'
     };
 
@@ -22,38 +50,29 @@ try {
     }
 
     const payload = Github.context.payload;
-
-    if (payload.action !== "ready_for_review" && payload.action !== "opened") {
-        return
-    }
-
+    const review = payload.review;
     const pr = payload.pull_request;
-    const slack = new Slack(config.hookUrl);
-    // console.log(JSON.stringify(payload, null, 2));
+    let message;
 
-    if (payload.pull_request.draft && config.ignoreDrafts === true) {
+    if (!review && payload.action !== "ready_for_review" && payload.action !== "opened") {
         return
     }
 
-    let message = config.format;
-    config.format.match(/\{.+\}/g).forEach(template => {
-        const templateWithoutBrackets = template
-            .replace(/^\{\s?/, "")
-            .replace(/\s?\}$/, "");
+    const slack = new Slack(config.hookUrl);
 
-        const keys = templateWithoutBrackets.split(".")
-        console.log(keys)
-        let value = payload;
-        keys.forEach(key => {
-            try {
-                value = value[key];
-            } catch (error) {
-                console.log(error);
-            }
-        });
-        console.log(value);
-        message = message.replace(template, value);
-    });
+    if (review) {
+        if (payload.review.state == "approved") {
+            message = fillTemplate(payload, config.pr_approved_format);
+        } else if (payload.review.state == "changes_requested") {
+            message = fillTemplate(payload, config.pr_rejected_format);
+        }
+    } else {
+        if (pr.draft && config.ignoreDrafts === true) {
+            return
+        }
+
+        message = fillTemplate(payload, config.pr_ready_for_review_format);
+    }
 
     slack.send({
         text: message,
